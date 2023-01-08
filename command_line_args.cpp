16719241819550094fae0728ae0afa376e1f72eb
Lorcan Nicholls <ln356@cam.ac.uk>
2023-01-08 13:00:34 +0800

    added new one

---
+++ command_line_args.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;  // can shorten e.g. std::cout to just cout
+
+// type & .\"command_line_args.exe" followed by some args in CLI to run
+
+int main(int argc, char **argv) {
+    cout << "Number of args passed: " << argc << endl;
+
+    for (int i = 0; i < argc; i++)  // for loop starting from 0, going to argc, incrementing
+        cout << "Passed in " << argv[i] << endl;
+
+}
--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,11 @@
 #include <iostream>  // a library for input and output
 
-int main() {  // main function, always the entry point for program
+/*
+Multiline
+comment
+*/
+
+int main(int arc, char **argv) {  // main function, always the entry point for program
     std::cout << "Hello, World!" << std::endl;  // print string
     return 0;  // best practice to return 0 when OK
 }
