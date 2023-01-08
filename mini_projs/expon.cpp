1bdc192091e43d04531acfe655cf7524e78628bb
Lorcan Nicholls <ln356@cam.ac.uk>
2023-01-08 20:24:08 +0800

    fixed

--- mini_projs/expon.cpp
+++ mini_projs/expon.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
-#include <format>
+#include <math.h>
+#include <iomanip>
 #include <string>
 
 // reimplement the e^x function for all real x using the Maclaurin series
 
-long double exp(long double x) {
+long double my_exp(long double x) {  // maximise precision using long doubles
 
     long double total = 0;
     long double prod = 1;
     int n = 0;
 
     do {
-        prod *= (2.0 / (n == 0 ? 1 : n));  // prevent inaccuracy by multiplying cumulatively
+        prod *= (n == 0 ? 1 : x / n);  // ternary operator for avoiding division by zero
         total += prod;
         n++;
-    } while (prod >= 1e-15);
+    } while (fabs(prod) >= 1e-15);  // answer tolerance
 
     return total;
 }
 
 int main() {
     
+    std::cout << std::setprecision(16);  // set viewing precision
     std::string x_str;
-    std::cout << "Enter a number to take the exponential of: " << std::endl;
-    std::cin >> x_str;
-    long double x = std::stold(x_str);
-    long double y = exp(x);
-    std::cout << "The exponential of " << x << " is " << y << std::endl;
+    while (true) {
+        std::cout << "Enter a number to take the exponential of: ";
+        std::cin >> x_str;
+        long double x = std::stold(x_str);
+        long double y = my_exp(x);
+        std::cout << "The exponential of " << x << " is " << y << std::endl;
+    }
 }
