b722d1badc0f3407008899cff7602314b837a1bc
Lorcan Nicholls <ln356@cam.ac.uk>
2023-01-08 13:02:02 +0800

    more comments

--- command_line_args.cpp
+++ command_line_args.cpp
@@ -4,7 +4,7 @@ using namespace std;  // can shorten e.g. std::cout to just cout
 
 // type & .\"command_line_args.exe" followed by some args in CLI to run
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) {  // argc: argument count, argv: argument vector (character array / string)
     cout << "Number of args passed: " << argc << endl;
 
     for (int i = 0; i < argc; i++)  // for loop starting from 0, going to argc, incrementing
--- hello_world.cpp
+++ hello_world.cpp
@@ -5,7 +5,7 @@ Multiline
 comment
 */
 
-int main(int arc, char **argv) {  // main function, always the entry point for program
+int main() {  // main function, always the entry point for program
     std::cout << "Hello, World!" << std::endl;  // print string
     return 0;  // best practice to return 0 when OK
 }
